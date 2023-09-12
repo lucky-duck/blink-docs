3935249f078ba2ad2220f76ef105dc97546dcf04
Sam Ducker <sam@luckyduck.digital>
2023-09-12 17:50:03 +0100

    Style enhancements to homepage

--- src/components/Homepage/Feature/index.tsx
+++ src/components/Homepage/Feature/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles.module.css";
 import FullRightArrowIcon from "../../Svg/FullRightArrowIcon";
 
 type Props = {
@@ -10,76 +11,16 @@ type Props = {
 
 export default function Feature({ title, description, Icon, link }: Props) {
   return (
-    <div style={featureWrapper}>
-      <div style={featureInnerWrapper}>
-        <div style={iconWrapper}>{Icon && <Icon style={iconStyle} />}</div>
-        <h3 style={titleStyle}>{title}</h3>
-        <p style={descriptionStyle}>{description}</p>
+    <a className={styles.featureWrapper} href={link}>
+      <div className={styles.featureInnerWrapper}>
+        <div className={styles.iconWrapper}>{Icon && <Icon className={styles.iconStyle} />}</div>
+        <h3 className={styles.titleStyle}>{title}</h3>
+        <p className={styles.descriptionStyle}>{description}</p>
       </div>
-      <a style={button} href={link}>
+      <div className={styles.button}>
         <span>Learn more</span>
         <FullRightArrowIcon />
-      </a>
-    </div>
+      </div>
+    </a>
   );
 }
-
-const featureWrapper: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-between",
-  alignItems: "flex-start",
-  gap: "0.5rem",
-  padding: "2.5rem 2.5rem",
-  width: "calc((100% - 4rem) / 3)",
-  border: "1px solid #D9DBE6",
-  height: "21rem",
-  borderRadius: "1rem",
-};
-
-const featureInnerWrapper: React.CSSProperties = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "flex-start",
-  gap: "1.2rem",
-};
-
-const iconWrapper: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: "#F9FAFB",
-  padding: "1.2rem",
-  borderRadius: "0.5rem",
-};
-
-const iconStyle: React.CSSProperties = {
-  width: "1.3rem",
-  height: "1.3rem",
-};
-
-const titleStyle: React.CSSProperties = {
-  fontSize: "1.5rem",
-  fontWeight: 600,
-  margin: 0,
-};
-
-const descriptionStyle: React.CSSProperties = {
-  fontSize: "1rem",
-  maxWidth: "20rem",
-  fontWeight: 400,
-};
-
-const button: React.CSSProperties = {
-  color: "#4467FB",
-  borderRadius: "0.5rem",
-  fontSize: "1rem",
-  fontWeight: 600,
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  gap: "0.5rem",
-  transition: "all 0.2s ease-in-out",
-};

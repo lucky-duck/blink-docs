0c1cca373d39a204cd431935f22fdfff28d43a38
Sam Ducker <sam@luckyduck.digital>
2023-09-27 21:06:34 +0100

    Add proxy to all apis

--- api/proxy/[...url].ts
+++ api/proxy/[...url].ts
@@ -5,9 +5,6 @@ export default async (req: VercelRequest, res: VercelResponse) => {
   // Extract the target URL from the request path
   let targetURL = req.query["...url"] as string;
 
-  console.log(req.headers["content-type"]);
-  console.log(req.headers["authorization"]);
-
   // Convert hyphens back to slashes after .co.uk
   targetURL = targetURL.replace(/\.co\.uk-/, ".co.uk/").replace(/-/g, "/");
 

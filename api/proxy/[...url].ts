6a9559f8f37b36e6f7569e4b9ca0ec2ccbc3b7ab
Sam Ducker <sam@luckyduck.digital>
2023-09-27 19:30:22 +0100

    go back to proxy api route

---
+++ api/proxy/[...url].ts
@@ -0,0 +1,40 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import axios, { AxiosRequestHeaders } from "axios";
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  // Extract the target URL from the request path
+  let targetURL = req.query.path.toString();
+
+  // Check if the URL doesn't start with 'http' (assuming it could be http or https) and prepend it
+  if (!targetURL.startsWith("http")) {
+    targetURL = "https://" + targetURL;
+  }
+
+  // Prepare headers, removing problematic ones
+  const { host, referer, origin, ...cleanedHeaders } = req.headers;
+
+  // Convert headers to Axios-compatible format
+  const axiosHeaders: AxiosRequestHeaders = {};
+  for (const key in cleanedHeaders) {
+    const value = cleanedHeaders[key];
+    if (Array.isArray(value)) {
+      axiosHeaders[key] = value.join("; ");
+    } else if (typeof value === "string") {
+      axiosHeaders[key] = value;
+    }
+  }
+
+  try {
+    const response = await axios({
+      method: req.method as any,
+      url: targetURL,
+      headers: axiosHeaders,
+      data: req.body,
+    });
+
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    console.log("Error in proxying request:", error.message);
+    return res.status(error.response?.status || 500).json({ error: error.message });
+  }
+};
